fde3dceeb722dbbc76c8ea22f10bb1e4d0fbefdd
vitor_marthendal <vrn@neo.ufsc.br>
2020-07-17 11:30:33 -0300

    update model to cadCAD 4.x version

--- Compartmental_Models/demos/covid-19/covid_19_seird_predict/model/sys_params.py
+++ Compartmental_Models/demos/covid-19/covid_19_seird_predict/model/sys_params.py
@@ -1,6 +1,6 @@
 sys_params = {
         # 𝛽:  expected amount of people an infected person infects per day
-        'infection_rate': [.5/14, .8/14, 1.1/14],        
+        'infection_rate': [0.035714, 0.057143, 0.078571, 0.1],        
         # 𝛾: the proportion of infected recovering per day ( 𝛾  = 1/D)
         'recovering_rate': [1/14],
         # 𝛿: expected rate that exposed people turn into infected
