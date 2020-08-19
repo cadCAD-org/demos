27d7530b541ced017357900a6d2d99fc274d5b34
Pedro Kretzschmar <pedro.kretz@gmail.com>
2020-08-19 11:51:46 -0300

    Modified the files to make the error easier to reproduce.

--- uniswap/config.py
+++ uniswap/config.py
@@ -1,10 +1,22 @@
 from cadCAD.configuration import Experiment
 from cadCAD.configuration.utils import config_sim
-from .model.state_variables import genesis_states
+# from .model.state_variables import genesis_states
 from .model.partial_state_update_block import PSUBs
-from .model.sys_params import sys_params as sys_params
-from .sim_params import SIMULATION_TIME_STEPS
+# from .model.sys_params import sys_params as sys_params
+# from .sim_params import *
 
+sys_params = {
+    'fee_numerator': [997],
+    'fee_denominator': [1000]
+}
+
+genesis_states = {
+    'DAI_balance': 5900000000000000000000,
+    'ETH_balance': 30000000000000000000,
+    'UNI_supply': 30000000000000000000
+}
+
+SIMULATION_TIME_STEPS = 100
 MONTE_CARLO_RUNS = 1
 
 sim_config = config_sim(
