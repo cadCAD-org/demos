66e06b98aa1c0a7b2acb80daee65d5f7c3e3d9da
Pedro Kretzschmar <pedro.kretz@gmail.com>
2020-08-21 17:00:12 -0300

    Reset the files to original format

--- uniswap/config.py
+++ uniswap/config.py
@@ -1,23 +1,9 @@
 from cadCAD.configuration import Experiment
 from cadCAD.configuration.utils import config_sim
-# from .model.state_variables import genesis_states
+from .model.state_variables import genesis_states
 from .model.partial_state_update_block import PSUBs
-# from .model.sys_params import sys_params as sys_params
-# from .sim_params import *
-
-sys_params = {
-    'fee_numerator': [997],
-    'fee_denominator': [1000]
-}
-
-genesis_states = {
-    'DAI_balance': 5900000000000000000000,
-    'ETH_balance': 30000000000000000000,
-    'UNI_supply': 30000000000000000000
-}
-
-SIMULATION_TIME_STEPS = 100
-MONTE_CARLO_RUNS = 1
+from .model.sys_params import sys_params as sys_params
+from .sim_params import *
 
 sim_config = config_sim(
     {
