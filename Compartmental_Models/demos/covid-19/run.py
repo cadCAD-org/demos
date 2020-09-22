2e286b8aa4bf5227cbfa41fb215d66a3ef231828
Stefenon <victorstefenon0@gmail.com>
2020-09-22 18:08:37 -0300

    Correct run.py script

--- Compartmental_Models/demos/covid-19/run.py
+++ Compartmental_Models/demos/covid-19/run.py
@@ -5,9 +5,9 @@ from cadCAD.engine import ExecutionMode, ExecutionContext, Executor
 from covid_19_sir import config
 from covid_19_seir import config
 from covid_19_seird import config
+from covid_19_seir_3778 import config
 from covid_19_stochastic import config
 
-#from {new_simulation} import config
 
 from cadCAD import configs
 import pandas as pd
@@ -18,9 +18,8 @@ def run(drop_midsteps: bool=True) -> pd.DataFrame:
     Run all experiments and return their output on the dataset column.
     Each line represents an iteration of the parameter-sweep combinations.
     """
-    
     exec_mode = ExecutionMode()
-    multi_mode_ctx = ExecutionContext(context=exec_mode.multi_mode)
+    multi_mode_ctx = ExecutionContext(context=exec_mode.multi_proc)
     run = Executor(exec_context=multi_mode_ctx, configs=configs)
     raw_result, _, _ = run.execute()
     results = pd.DataFrame(raw_result)
