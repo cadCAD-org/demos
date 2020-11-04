66b6432850a27de6d6814963bf5dad3121caefbd
Pedro Kretzschmar <pedro.kretz@gmail.com>
2020-11-04 10:01:05 -0300

    Add fix cost to sys params

--- uniswap/model/sys_params.py
+++ uniswap/model/sys_params.py
@@ -1,9 +1,10 @@
 import pandas as pd
 
 sys_params = {
-    'fee_numerator': [997, 995, 990, 970, 900],
+    'fee_numerator': [997],
     'fee_denominator': [1000],
     'uniswap_events': [pd.read_pickle('uniswap_events.pickle')],
     'c_rule': [13],
-    'conv_tolerance': [0.05]
+    'conv_tolerance': [0.05],
+    'fix_cost': [1]
 }
