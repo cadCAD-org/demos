9d4ed7c2fe14a81730afbacce1ad39278b98816e
Pedro Kretzschmar <pedro.kretz@gmail.com>
2020-11-04 09:46:39 -0300

    Add unprofitable profit to arbitrage trade

--- uniswap/model/parts/policy_aux.py
+++ uniswap/model/parts/policy_aux.py
@@ -1,6 +1,5 @@
 from math import sqrt
 
-
 def get_parameters(uniswap_events, event, s, t):
     if(event == "TokenPurchase"):
         I_t = s['ETH_balance']
@@ -8,7 +7,7 @@ def get_parameters(uniswap_events, event, s, t):
         I_t1 = uniswap_events['eth_balance'][t]
         O_t1 = uniswap_events['token_balance'][t]
         delta_I = uniswap_events['eth_delta'][t]
-        delta_O = abs(uniswap_events['token_delta'][t])
+        delta_O = uniswap_events['token_delta'][t]
         action_key = 'eth_sold'
     else:
         I_t = s['DAI_balance']
@@ -16,7 +15,7 @@ def get_parameters(uniswap_events, event, s, t):
         I_t1 = uniswap_events['token_balance'][t]
         O_t1 = uniswap_events['eth_balance'][t]
         delta_I = uniswap_events['token_delta'][t]
-        delta_O = abs(uniswap_events['eth_delta'][t])
+        delta_O = uniswap_events['eth_delta'][t]
         action_key = 'tokens_sold'
     
     return I_t, O_t, I_t1, O_t1, delta_I, delta_O, action_key
@@ -32,9 +31,9 @@ def get_input_price(delta_I, I_t, O_t, _params):
     fee_numerator = _params['fee_numerator']
     fee_denominator = _params['fee_denominator']
     delta_I_with_fee = delta_I * fee_numerator
-    numerator = delta_I_with_fee * O_t                        
-    denominator = (I_t * fee_denominator) + delta_I_with_fee 
-    return int(numerator // denominator)                      
+    numerator = delta_I_with_fee * O_t
+    denominator = (I_t * fee_denominator) + delta_I_with_fee
+    return int(numerator // denominator)
 
 def classifier(delta_I, delta_O, c_rule):
     if (delta_I / (10 ** (18-c_rule))).is_integer() or (delta_O / (10 ** (18-c_rule))).is_integer() :
@@ -52,3 +51,33 @@ def get_delta_I(P, I_t, O_t, _params):
     )  / (2*a)
 
     return int(delta_I)
+
+def unprofitable_transaction(t, type_of, P, actual_P, delta_I, delta_O, action_key, convert_rate, _params):
+    fix_cost = int((_params[0]['fix_cost']/convert_rate)*(10**18))
+    if(delta_O < 0):
+        print(delta_O)
+    if(action_key == 'eth_sold'):
+        profit = delta_O*P - (delta_I)
+        # if(t<50):
+            # print(action_key)
+            # print("is inverted:     ", type_of)
+            # print("actual_P: ", actual_P)
+            # print("P       : ", P)
+            # print("delta_I    (ETH): ", delta_I)
+            # print("delta_O*P  (ETH): ", int(delta_O*P))
+            # print("abs_profit (ETH): ", profit)
+            # print("fix_cost   (ETH): ", fix_cost)
+            # print("abs_profit (USD): ", (profit/(10**18))*convert_rate)
+    else:
+        profit = delta_O - int(delta_I/P)
+        # if(t<50):
+            # print(action_key)
+            # print("is inverted:     ", type_of)
+            # print("actual_P: ", actual_P)
+            # print("P       : ", P)
+            # print("delta_I/P  (ETH): ", int(delta_I/P))
+            # print("delta_O    (ETH): ", delta_O)
+            # print("abs_profit (ETH): ", profit)
+            # print("fix_cost   (ETH): ", fix_cost)
+            # print("abs_profit (USD): ", (profit/(10**18))*convert_rate)
+    return (profit < fix_cost)
--- uniswap/model/parts/uniswap_model.py
+++ uniswap/model/parts/uniswap_model.py
@@ -1,12 +1,10 @@
 from .policy_aux import *
-from .UNI_aux import *
-from .DAI_aux import *
-from .ETH_aux import *
+from .suf_aux import *
 
 # Policy
 
 def p_actionDecoder(_params, substep, sH, s):
-    uniswap_events = _params['uniswap_events']
+    uniswap_events = _params[0]['uniswap_events']
     
     prev_timestep = s['timestep']
     if substep > 1:
@@ -19,8 +17,7 @@ def p_actionDecoder(_params, substep, sH, s):
         'eth_sold': 0,
         'tokens_sold': 0,
         'eth_deposit': 0,
-        'UNI_burn': 0, 
-        'UNI_pct': 0,
+        'UNI_burn': 0,        
     }
 
     #Event variables
@@ -29,63 +26,79 @@ def p_actionDecoder(_params, substep, sH, s):
 
     if event in ['TokenPurchase', 'EthPurchase']:
         I_t, O_t, I_t1, O_t1, delta_I, delta_O, action_key = get_parameters(uniswap_events, event, s, t)
-        if _params['c_rule'] == -1:
-            action[action_key] = delta_I
-        elif classifier(delta_I, delta_O, _params['c_rule']) == "Conv":            #Convenience trader case
-            calculated_delta_O = int(get_input_price(delta_I, I_t, O_t, _params))
-            if calculated_delta_O >= delta_O * (1-_params['conv_tolerance']):
+        if classifier(delta_I, delta_O, _params[0]['c_rule']) == "Conv":
+            calculated_delta_O = int(get_input_price(delta_I, I_t, O_t, _params[0]))
+            historic_delta_O = int(get_input_price(delta_I, I_t1, O_t1, _params[0]))
+            if calculated_delta_O >= historic_delta_O * (1 - _params[0]['conv_tolerance']):
                 action[action_key] = delta_I
-            else:
-                action[action_key] = 0
-        else:            #Arbitrary trader case
+        else:
             P = I_t1 / O_t1
             actual_P = I_t / O_t
             if(actual_P > P):
                 I_t, O_t, I_t1, O_t1, delta_I, delta_O, action_key = get_parameters(uniswap_events, reverse_event(event), s, t)
-            delta_I = get_delta_I(P, I_t, O_t, _params)
-            action[action_key] = delta_I
+                P = I_t1 / O_t1
+                actual_P = I_t / O_t
+                delta_I = get_delta_I(P, I_t, O_t, _params[0])
+                delta_O = get_input_price(delta_I, I_t, O_t, _params[0])
+                if(unprofitable_transaction(t, "inverted", P, actual_P, delta_I, delta_O, action_key, uniswap_events['convert_rate'][t], _params)):
+                    delta_I = 0
+                action[action_key] = delta_I
+            else:
+                delta_I = get_delta_I(P, I_t, O_t, _params[0])
+                delta_O = get_input_price(delta_I, I_t, O_t, _params[0])
+                if(unprofitable_transaction(t, "normal", P, actual_P, delta_I, delta_O, action_key, uniswap_events['convert_rate'][t], _params)):
+                    delta_I = 0
+                action[action_key] = delta_I
     elif event == 'AddLiquidity':
         delta_I = uniswap_events['eth_delta'][t]
         action['eth_deposit'] = delta_I
     elif event == 'Transfer':
         UNI_delta = uniswap_events['uni_delta'][t]
-        UNI_supply = uniswap_events['UNI_supply'][t-1]
         if UNI_delta < 0:
             action['UNI_burn'] = -UNI_delta
-            action['UNI_pct'] = -UNI_delta / UNI_supply
+
     del uniswap_events
     return action
+
+def profitable(P, delta_I, delta_O, action_key, _params):
+    gross_profit = (delta_O*P) - delta_I
+    if(action_key == 'token'):
+        convert_to_ETH = gross_profit/P
+        is_profitable = (convert_to_ETH > _params['fix_cost'])
+    else:
+        is_profitable = (gross_profit > _params['fix_cost'])
+
 
 # SUFs
 
 def s_mechanismHub_DAI(_params, substep, sH, s, _input):
     action = _input['action_id']
     if action == 'TokenPurchase':
-        return ethToToken_DAI(_params, substep, sH, s, _input)
+        return ethToToken_DAI(_params[0], substep, sH, s, _input)
     elif action == 'EthPurchase':
-        return tokenToEth_DAI(_params, substep, sH, s, _input)
+        return tokenToEth_DAI(_params[0], substep, sH, s, _input)
     elif action == 'AddLiquidity':
-        return addLiquidity_DAI(_params, substep, sH, s, _input)
+        return addLiquidity_DAI(_params[0], substep, sH, s, _input)
     elif action == 'Transfer':
-        return removeLiquidity_DAI(_params, substep, sH, s, _input)
+        return removeLiquidity_DAI(_params[0], substep, sH, s, _input)
     return('DAI_balance', s['DAI_balance'])
     
 def s_mechanismHub_ETH(_params, substep, sH, s, _input):
     action = _input['action_id']
     if action == 'TokenPurchase':
-        return ethToToken_ETH(_params, substep, sH, s, _input)
+        return ethToToken_ETH(_params[0], substep, sH, s, _input)
     elif action == 'EthPurchase':
-        return tokenToEth_ETH(_params, substep, sH, s, _input)
+        return tokenToEth_ETH(_params[0], substep, sH, s, _input)
     elif action == 'AddLiquidity':
-        return addLiquidity_ETH(_params, substep, sH, s, _input)
+        return addLiquidity_ETH(_params[0], substep, sH, s, _input)
     elif action == 'Transfer':
-        return removeLiquidity_ETH(_params, substep, sH, s, _input)
+        return removeLiquidity_ETH(_params[0], substep, sH, s, _input)
     return('ETH_balance', s['ETH_balance'])
 
 def s_mechanismHub_UNI(_params, substep, sH, s, _input):
     action = _input['action_id']
     if action == 'AddLiquidity':
-        return addLiquidity_UNI(_params, substep, sH, s, _input)
+        return addLiquidity_UNI(_params[0], substep, sH, s, _input)
     elif action == 'Transfer':
-        return removeLiquidity_UNI(_params, substep, sH, s, _input)
-    return('UNI_supply', s['UNI_supply'])
+        return removeLiquidity_UNI(_params[0], substep, sH, s, _input)
+    return('UNI_supply', s['UNI_supply'])   
