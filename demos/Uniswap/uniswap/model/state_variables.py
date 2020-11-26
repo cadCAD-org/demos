f5d3b3d20fd84152b914cc086cef4f4ebf5b4b7b
pkretzschmar <43218565+pkretzschmar@users.noreply.github.com>
2020-11-26 17:39:59 -0300

    Revert "Add uniswap demo"

--- demos/Uniswap/uniswap/model/state_variables.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-genesis_states = {
-    'DAI_balance': 5900000000000000000000,
-    'ETH_balance': 30000000000000000000,
-    'UNI_supply': 30000000000000000000,
-    'price_ratio': 0
-}
