61b9b53b8c24e19d08ec8b7598a2bc515880dd33
Jorao Jr <jorgomjr@gmail.com>
2022-05-15 13:10:47 +0200

    Fix Numerical Computation tutorial

--- tutorials/numerical_computation/scratch/model/run.py
+++ tutorials/numerical_computation/scratch/model/run.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from .sys_params import initial_values
-from model import config 
+from .config import exp
 from cadCAD.engine import ExecutionMode, ExecutionContext,Executor
 from cadCAD import configs
 
@@ -13,7 +13,7 @@ def run():
     exec_mode = ExecutionMode()
     local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
 
-    simulation = Executor(exec_context=local_mode_ctx, configs=configs)
+    simulation = Executor(exec_context=local_mode_ctx, configs=exp.configs)
     raw_system_events, tensor_field, sessions = simulation.execute()
     # Result System Events DataFrame
     df = pd.DataFrame(raw_system_events)
