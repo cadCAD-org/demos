94c50ccfbd0b3339fb3ec71f48c857fb77d365ce
Andrew Clark <andrewclark@Andrews-Air.fios-router.home>
2024-07-24 10:22:55 -0400

    initial push for moving over to cadCAD 0.5.3. One parameter sweep issue remains on robot-marbles-part-7.

--- demos/System_Dynamics/Compartment_Models/run.py
+++ demos/System_Dynamics/Compartment_Models/run.py
@@ -3,24 +3,27 @@ from cadCAD.engine import ExecutionMode, ExecutionContext, Executor
 
 # Simulation configs, input any new simulations here
 
-from sir import config
-from seir import config
-from seird import config
-from stochastic_seir import config
-
 from cadCAD import configs
 import pandas as pd
 
 
-def run(drop_midsteps: bool=True) -> pd.DataFrame:
-    """
-    Run all experiments and return their output on the dataset column.
-    Each line represents an iteration of the parameter-sweep combinations.
-    """
-    exec_mode = ExecutionMode()
 
-    multi_mode_ctx = ExecutionContext(context=exec_mode.multi_mode)
-    run = Executor(exec_context=multi_mode_ctx, configs=configs)
-    raw_result, tensor_field, sessions = run.execute()
-    results = pd.DataFrame(raw_result)
-    return results
+def run(sim):
+    '''
+    Definition:
+    Run simulation
+    '''
+    # Single
+    exec_mode = ExecutionMode()
+    local_mode_ctx = ExecutionContext(context=exec_mode.local_mode)
+
+    simulation = Executor(exec_context=local_mode_ctx, configs=sim.configs)
+    raw_system_events, tensor_field, sessions = simulation.execute()
+    # Result System Events DataFrame
+    df = pd.DataFrame(raw_system_events)
+    
+     # subset to last substep
+    df = df[df['substep'] == df.substep.max()]
+    
+    return df
+
